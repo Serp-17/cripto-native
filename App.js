8244ae75a5b4568e5d647ec9477a37b194dfe6bd
Sergey <sergey.k@tinyelephant.no>
2022-06-03 00:49:38 +0300

    add api.js

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import CoinMarketCap from './api';
+
+const apiKey = 'f4ffe871-1251-43a1-af9f-600bfa664b0b';
+const client = new CoinMarketCap(apiKey)
+
+const onclick = () => {
+  console.log(client.getQuotes({id: '1'}).then(console.log).catch(console.error)  )
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Button accessibilityLabel="Learn more about this purple button"/> */}
+      <Text>123w</Text>
+      <Button
+        onPress={onclick}
+        title="Learn More"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
       <StatusBar style="auto" />
     </View>
   );
